49bbb3abff679512017188c72109fa966925da85
GegeleDanila <gegeledanila@gmail.com>
2018-02-09 17:48:39 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+import YourComponent from 'components/YourComponent'
+
+class App extends Component {
+  render () {
+    return (
+      <YourComponent>asdasd</YourComponent>
+    )
+  }
+}
+
+export default App
